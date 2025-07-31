8885d93f29ac57b34b5d4615c4cb9d1be99e01fd
Khayitboev Khojiakbar <xayitbaevhojiakbar@gmail.com>
2025-08-01 00:11:50 +0200

    Set the metadata in stripe checkout session in payment intent

--- src/main/java/com/example/store/payments/StripePaymentGateway.java
+++ src/main/java/com/example/store/payments/StripePaymentGateway.java
@@ -32,7 +32,7 @@ public class StripePaymentGateway implements PaymentGateway{
                     .setMode(SessionCreateParams.Mode.PAYMENT)
                     .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
                     .setCancelUrl(websiteUrl + "/checkout-cancel")
-                    .putMetadata("order_id", order.getId().toString());
+                    .setPaymentIntentData(createPaymentIntentData(order));
             order.getItems().forEach(item -> {
                 var lineItem = createLineItem(item);
                 builder.addLineItem(lineItem);
@@ -44,6 +44,11 @@ public class StripePaymentGateway implements PaymentGateway{
             throw new PaymentException();
         }
     }
+
+    private static SessionCreateParams.PaymentIntentData createPaymentIntentData(Order order) {
+        return SessionCreateParams.PaymentIntentData.builder().putMetadata("order_id", order.getId().toString()).build();
+    }
+
     public Optional<PaymentResult> parseWebhookRequest(WebhookRequest request) {
         String payload = request.getPayload();
         String signature = request.getHeaders().get("stripe-signature");
